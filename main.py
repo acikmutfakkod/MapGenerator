494d3679d7e04c47bd9c5fc78dd5b1ea55f83499
acikmutfakkod <ardasezen@gmail.com>
2020-07-18 14:24:51 +0300

    Versiyon 0.0.3: Harita görüntülemek için ikinci yöntemdeki yapı FindPlace() sınıfı içerisindeki ilgili örnek metotları (instance methods) altına yeniden yazıldı. Koordinat girişi parametrik hale getirildi. Map() sınıfında belirlenen tile ve zoom parametreleri bu versiyonda da eksik.

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import folium as fo
 
 # Haritayı görüntülemek
-# Map() sınıfının örneğini (instance) oluşturur
-fo_map = fo.Map()  #openstreetmap #Mapbox Bright #stamenterrain #stamentoner #stamenwatercolor #cartodbpositron #CartoDB dark_matter
-# FeatureGroup() sınıfının örneğini (instance) oluşturur
-fgi = fo.FeatureGroup(name='OpenStreetMap')
-# FeatureGroup() sınıfının add_child() metoduna instance üzerinden erişerek harita üzerinde koordinatları verilen lokasyon işaretlenir
-fgi.add_child(fo.Marker(location=[39.931774, 32.838673], popup='varış yeri', icon=fo.Icon(color='red')))
-fo_map.add_child(fgi)
+class FindPlace():
 
+    def __init__(self):
+        # Map() sınıfının örneğini (instance) oluşturur
+        self.fo_map = fo.Map()  #openstreetmap #Mapbox Bright #stamenterrain #stamentoner #stamenwatercolor #cartodbpositron #CartoDB dark_matter
+        # FeatureGroup() sınıfının örneğini (instance) oluşturur
+        self.fgi = fo.FeatureGroup(name='OpenStreetMap')
+        self.x = 0.0
+        self.y = 0.0
 
-fo_map.save('map.html')
+        self.run()
+        self.show_map()
+
+    def mark_coordinates(self, x, y):
+        # FeatureGroup() sınıfının add_child() metoduna instance üzerinden erişerek harita üzerinde koordinatları verilen lokasyon işaretlenir
+        self.fgi.add_child(fo.Marker(location=[x, y], popup='varış yeri', icon=fo.Icon(color='red')))
+        self.fo_map.add_child(self.fgi)
+
+    def show_map(self):
+        self.fo_map.save('map.html')
+
+    def run(self):
+        self.x = float(input('İlk koordinatı girin: '))
+        self.y = float(input('İkinci koordinatı girin: '))
+        self.mark_coordinates(self.x, self.y)
+
+if __name__ == '__main__':
+    fp = FindPlace()
+

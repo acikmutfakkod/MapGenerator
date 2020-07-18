5bf9f982020dc541a7aa92df05761f0c95b19c3a
acikmutfakkod <ardasezen@gmail.com>
2020-07-18 13:34:19 +0300

    Versiyon 0.0.2: Harita görüntülemek için ikinci yöntem. Marker var fakat Map() sınıfında belirlenen tile ve zoom parametreleri eksik.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import folium as fo
 
 # Haritayı görüntülemek
-fo_map = fo.Map(location=[39.931774, 32.838673], zoom_start=13, tiles='openstreetmap')  #openstreetmap #Mapbox Bright #stamenterrain #stamentoner #stamenwatercolor #cartodbpositron #CartoDB dark_matter
+# Map() sınıfının örneğini (instance) oluşturur
+fo_map = fo.Map()  #openstreetmap #Mapbox Bright #stamenterrain #stamentoner #stamenwatercolor #cartodbpositron #CartoDB dark_matter
+# FeatureGroup() sınıfının örneğini (instance) oluşturur
+fgi = fo.FeatureGroup(name='OpenStreetMap')
+# FeatureGroup() sınıfının add_child() metoduna instance üzerinden erişerek harita üzerinde koordinatları verilen lokasyon işaretlenir
+fgi.add_child(fo.Marker(location=[39.931774, 32.838673], popup='varış yeri', icon=fo.Icon(color='red')))
+fo_map.add_child(fgi)
+
 
 fo_map.save('map.html')
